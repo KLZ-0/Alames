e573af378458e97320c69d860f1d52d0cea37ff0
KLZ-0 <adrian@kalazi.com>
2018-11-03 20:37:42 +0100

    Added success message

--- install-launcher.bat
+++ install-launcher.bat
@@ -2,6 +2,6 @@
 
 FOR /F "tokens=*" %%a in ('where pythonw') do SET pywpath=%%a
 Assoc .pyw=pywfile
-Ftype pywfile="%pywpath%" %1
+Ftype pywfile="%pywpath%" %1 && echo File association changed successfully
 
 pause
