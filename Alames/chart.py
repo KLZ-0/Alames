919b7577e5f63ea87facd52acdc0003ae2efc400
KLZ-0 <adrian@kalazi.com>
2019-01-14 19:06:02 +0100

    Merged ChartSetup into Chart

--- Alames/chart.py
+++ Alames/chart.py
@@ -12,16 +12,64 @@ from Alames import scope
 from Alames import chartview
 from Alames import leftwidget
 from Alames import chartmodifier
-from Alames import chartsetup
+from Alames import chartlineseries
 
-class Chart(QChart, chartmodifier.ChartModifier, chartsetup.ChartSetup):
+class Chart(QChart, chartmodifier.ChartModifier):
     """
     Purpose: setup and modify the QChart
     Manages the charting subsystem consisting of the ChartView, Properties and BottomWidget.
     Initializes the required objects as its own properties
     """
+    ydata = []
+    xdata = []
+
     def __init__(self):
         super(Chart, self).__init__()
+
+######## Setup
+
+    def constructChart(self, fileName):
+        self.setAcceptHoverEvents(True)
+
+        self.loadCSV(fileName)
+        self.fillSeries()
+        self.fillChart()
+        self.updateAxes()
+
+    def loadCSV(self, lFileName):
+        self.ydata = []
+        self.xdata = []
+
+        if lFileName.endswith(".csv.xz"):
+            try:
+                lFileName = lzma.open(lFileName) # file name or object
+            except lzma.LZMAError:
+                scope.errorPopup("LZMA decompression failed - damaged xz file")
+
+        f = pandas.read_csv(lFileName)
+        csv = f.values
+        self.columnNames = f.columns
+
+        for i in range(len(csv[0])-1):
+            self.ydata.append([])
+        for row in csv:
+            self.xdata.append(row[0])
+            for i in range(len(row)-1):
+                self.ydata[i].append(row[i+1])
+
+    def fillSeries(self):
+        self.qseries = []
+        for i in range(len(self.ydata)):
+            self.qseries.append(chartlineseries.ChartLineSeries(self.ydata[i]))
+
+            # self.qseries[-1].setUseOpenGL(True)
+            # IDEA: make a setting to turn off automatic header detection
+            self.qseries[i].setName(str(i+1) + " - " + self.columnNames[i+1])
+            # self.qseries[i].setName(str(i+1))
+
+    def fillChart(self):
+        for serie in self.qseries:
+            self.addSeries(serie)
 
 ######## Getters
 
